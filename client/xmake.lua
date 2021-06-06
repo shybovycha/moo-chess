fcf12eaf17c06b544339b03af05c340794f0b01b
Artem Shoobovych <shybovycha@gmail.com>
2021-06-06 21:45:17 +1000

    Observe how easy it is to add library with xmake

--- client/xmake.lua
+++ client/xmake.lua
@@ -1,8 +1,9 @@
-add_requires("sfml ~2.5.1")
+add_requires("sfml ~2.5.1", "tinyxml2 ~8.0.0")
 
 target("moo-chess-client")
     set_kind("binary")
+    set_languages("c++17")
 
     add_files("src/*.cpp")
 
-    add_packages("sfml")
+    add_packages("sfml", "tinyxml2")
