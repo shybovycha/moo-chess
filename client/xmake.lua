63e194fc26bc5df1f316b8ab708f8dee1577305c
Artem Shubovych <artem.shubovych@thetradedesk.com>
2023-03-16 11:42:54 +1100

    Rename data dir to assets

--- client/xmake.lua
+++
@@ -1,9 +0,0 @@
-add_requires("sfml ~2.5.1", "tinyxml2 ~8.0.0")
-
-target("moo-chess-client")
-    set_kind("binary")
-    set_languages("c++17")
-
-    add_files("src/*.cpp")
-
-    add_packages("sfml", "tinyxml2")
