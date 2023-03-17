3bc2f3742d40bdbca30c357a54b11f14efba90bd
Artem Shoobovych <shybovycha@gmail.com>
2023-03-17 20:29:06 +1100

    Add GTest

--- lib/src/test.cpp
+++ lib/src/test.cpp
@@ -3,15 +3,21 @@
 #include <string>
 #include <vector>
 
+#include <gtest/gtest.h>
+
 #include "Game.hpp"
 
-int main() {
+TEST(FENTest, BoardParsing) {
     auto game = std::make_unique<Game>();
 
-    game->parseFEN("rbnqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RBNQKBNR w KQkq -");
+    game->parseFEN("rbnqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RBNQKBNR w KQkq - 0 1");
 
     game->pieces[1][4] = NONE;
     game->pieces[3][4] = WHITE_PAWN;
 
-    std::cout << game->serializeAsFEN();
+    EXPECT_EQ(game->serializeAsFEN(), "rbnqkbnr/pppp1ppp/8/4P3/8/8/PPPPPPPP/RBNQKBNR w - - 0 1");
+}
+
+int main() {
+    RUN_ALL_TESTS();
 }
